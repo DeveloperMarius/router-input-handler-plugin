647e28c1ea806617bfd26f8d380051c20c261dc1
Marius Karstedt <m.karstedt@repaste.de>
2022-02-02 16:34:22 +0100

    + InputValidation in a middleware

--- src/InputHandler/TestMiddleware.php
+++ src/InputHandler/TestMiddleware.php
@@ -8,6 +8,14 @@ class TestMiddleware implements \Pecee\Http\Middleware\IMiddleware
 {
     public function handle(Request $request) : void
     {
+        /*
+         * Example of InputValidation in a middleware:
+         * inputHandler()->requireParameters(array(
+         *    'userId' => function(InputItem $value){
+         *        return $value->validate()->require()->isInteger()->valid();
+         *    }
+         * ));
+         */
         echo 'Success Middleware' . PHP_EOL;
     }
 
